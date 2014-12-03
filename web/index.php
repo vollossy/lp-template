1f4217b75f9c355aa5c7deaf6fd28aba0bcc9b06
Roman <vollossy@gmail.com>
2014-12-03 23:23:37 +0300

    Добавил отправку email при отправке заявки.

--- web/index.php
+++ web/index.php
@@ -1,18 +1,12 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+$config = json_decode(file_get_contents(__DIR__.'/../config.json'),true);
 $app = new Silex\Application();
 
 #region config
-$app['debug'] = true;
-$app['swiftmailer.options'] = array(
-    'host' => 'host',
-    'port' => '25',
-    'username' => 'username',
-    'password' => 'password',
-    'encryption' => null,
-    'auth_mode' => null
-);
+$app['debug'] = $config['debug'];
+$app['swiftmailer.options'] = $config['swiftmailer']['options'];
 #endregion
 
 $app->register(new \Silex\Provider\TwigServiceProvider(),array(
@@ -30,7 +24,7 @@ $app->get('/', function() use($app, $twig){
     return $twig->render('front/index.twig');
 });
 
-$app->post('/contact', function(\Symfony\Component\HttpFoundation\Request $request)use($mailer, $twig){
+$app->post('/contact', function(\Symfony\Component\HttpFoundation\Request $request)use($mailer, $twig, $config){
     $name = $request->get('name');
     $email = $request->get('email');
     $phone = $request->get('phone');
@@ -39,9 +33,9 @@ $app->post('/contact', function(\Symfony\Component\HttpFoundation\Request $reque
 
     $body = $twig->render('mail/contact.twig', $viewParams);
 
-    $swiftMessage = Swift_Message::newInstance("Новая заявка!", $body, 'text/html');
-    $swiftMessage->setFrom('info@example.com');
-    $swiftMessage->setTo('info@example.com');
+    $swiftMessage = Swift_Message::newInstance($config['message']['subject'], $body, 'text/html');
+    $swiftMessage->setFrom($config['system_email']);
+    $swiftMessage->setTo($config['system_email']);
 
     $mailer->send($swiftMessage);
 
